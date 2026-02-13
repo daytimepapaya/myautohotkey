14a7e00c0125ab0ce0cf03080a99ac94337a005d
daytimepapaya <93229662+daytimepapaya@users.noreply.github.com>
2026-02-13 09:21:56 +0900

    Update delete-garbage.ps1

--- delete-garbage.ps1
+++ delete-garbage.ps1
@@ -1,7 +1,7 @@
 $base_dir = ""
 $limit = (Get-Date).AddDays(-7)
 
-Get-ChildItem -Recurse -File -Exclude *.xlsx | 
+Get-ChildItem -Path $base_dir -Recurse -File -Exclude *.xlsx | 
     Where-Object { $_.Length -gt 1mb } | 
     Where-Object { $_.CreationTime -lt $limit } |
     Remove-Item

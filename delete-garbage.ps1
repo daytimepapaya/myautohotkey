95a2cf88efbe12de146c7179fb88c042b3d4ceaa
daytimepapaya <93229662+daytimepapaya@users.noreply.github.com>
2026-04-07 10:21:02 +0900

    typo

--- delete-garbage.ps1
+++ delete-garbage.ps1
@@ -1,9 +1,9 @@
 $day_limit = (Get-Date).AddDays(-3) # 直近3日
 $size_limit = "1mb" # サイズ。指定より大きいファイルを削除
-$execlude_files = "*.xlsx,*.bat,*.txt" # 削除対象除外ファイル
+$exclude_files = "*.xlsx,*.bat,*.txt" # 削除対象除外ファイル
 
 function delete_garbage($dir) {
-    Get-ChildItem -Path $dir -Recurse -File -Exclude $execlude_files | 
+    Get-ChildItem -Path $dir -Recurse -File -Exclude $exclude_files | 
     Where-Object { $_.Length -gt $size_limit } | 
     Where-Object { $_.CreationTime -lt $day_limit } |
     Remove-Item
